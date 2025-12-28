1e9edd8a9dbece37a842dd5d452fe469c93247fc
settipalli <arunareddy3144@gmail.com>
2025-12-28 08:38:39 +0530

    Bank-Management-System-Project

---
+++ src/bank/management/system/Conn.java
@@ -0,0 +1,20 @@
+
+package bank.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+   
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","aruna123");
+            s = c.createStatement();
+            
+        }catch(Exception e){
+                    System.out.println(e);
+                    }
+    }
+    
+}
